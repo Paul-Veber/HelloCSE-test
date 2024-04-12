a703cebad9411462caff3f6dab7188de6c04bcea
Paul veber <paul.veber56@gmail.com>
2024-04-12 13:15:25 +0200

    change default ProfileController to AdminProfileController

--- app/Repository/ProfilRepository.php
+++ app/Repository/ProfilRepository.php
@@ -10,16 +10,21 @@ class ProfilRepository
     public function create(array $data): bool
     {
         $profil = new Profil();
-        $profil->name = $data['name'];
-        $profil->email = $data['email'];
+        $profil->firstname = $data['first_name'];
+        $profil->lastname = $data['last_name'];
+        $profil->image = $data['image'];
+        $profil->status = $data['status'];
+
         return $profil->save();
     }
 
     public function update(array $data, Profil $profil): bool
     {
-        $profil->name = $data['name'];
-        $profil->email = $data['email'];
-        return $profil->save();
+        $profil->firstname = $data['first_name'] ?? $profil->firstname;
+        $profil->lastname = $data['last_name'] ?? $profil->lastname;
+        $profil->image = $data['image'] ?? $profil->image;
+        $profil->status = $data['status'] ?? $profil->status;
+        return $profil->update();
     }
 
     public function delete(Profil $profil): bool | null
@@ -27,7 +32,12 @@ class ProfilRepository
         return $profil->delete();
     }
 
-    public function all(): Collection
+    public function publicAll(): Collection
+    {
+        return Profil::all()->where('status', 'active')->select("id", "firstname", "lastname", "image");
+    }
+
+    public function adminAll(): Collection
     {
         return Profil::all();
     }
