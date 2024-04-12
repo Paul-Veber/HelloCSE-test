a8bb053b83b01b834cb02e697a250b021e279205
Paul veber <paul.veber56@gmail.com>
2024-04-12 15:41:04 +0200

    typo profil to profile

--- database/migrations/2024_04_11_211910_create_profiles_table.php
+++ database/migrations/2024_04_11_211910_create_profiles_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('profils', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->string("firstname");
             $table->string("lastname");
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('profils');
+        Schema::dropIfExists('profiles');
     }
 };
