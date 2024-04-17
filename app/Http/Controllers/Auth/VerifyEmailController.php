bbb60b4b99bca45fd65562697a79818052f7a1d5
Paul veber <paul.veber56@gmail.com>
2024-04-17 13:42:23 +0200

    better typing for VerifyEmailController

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -19,8 +19,11 @@ class VerifyEmailController extends Controller
         }
 
         if ($request->user()->markEmailAsVerified()) {
-            // @phpstan-ignore-next-line
-            event(new Verified($request->user()));
+            /**
+             * @var \Illuminate\Contracts\Auth\MustVerifyEmail $user
+             */
+            $user = $request->user();
+            event(new Verified($user));
         }
 
         return redirect()->intended(route('dashboard', absolute: false) . '?verified=1');
